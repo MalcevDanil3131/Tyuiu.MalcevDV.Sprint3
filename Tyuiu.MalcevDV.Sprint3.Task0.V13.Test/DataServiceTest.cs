51dd7415ca496ab9550df7634da7c3b67862fbe1
MalcevDanil3131 <strawmorrron666@gmail.com>
2025-10-31 21:13:28 +0500

    Add project files.

---
+++ Tyuiu.MalcevDV.Sprint3.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MalcevDV.Sprint3.Task0.V13.Lib;
+
+namespace Tyuiu.MalcevDV.Sprint3.Task0.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Checkvalid()
+        {
+            DataService ds = new DataService();
+
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 20;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 0.298;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MalcevDV.Sprint3.Task0.V13/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.MalcevDV.Sprint3.Task0.V13.Lib;
+void PrintCenteredLine(string text, int totalWidth)
+{
+    var padding = totalWidth - text.Length - 2; // -2 для звездочек по бокам
+    Console.WriteLine($"* {text}{new string(' ', padding)}*");
+}
+
+var width = 75;
+
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("Спринт #2", width);
+PrintCenteredLine("Тема: Создание итогового решения по спринту", width);
+PrintCenteredLine("Задание #7", width);
+PrintCenteredLine("Вариант #6", width);
+PrintCenteredLine("Выполнил: Мальцев Данил Вячеславович | РППБ-25-1", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("УСЛОВИЕ:", width);
+PrintCenteredLine("Написать программу, которое вычисляет сумму ряда", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+Console.WriteLine("Введите :", width);
+DataService ds = new DataService();
+double value = 0.5;
+int startValue = 1;
+int stopValue = 20;
+Console.WriteLine("Переменная X = " + value);
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+var res = ds.GetSumSeries(value, startValue, stopValue);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine($"{res}", width);
+Console.WriteLine(new string('*', width));
