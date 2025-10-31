a99733a1ebb04c7b83156f481446a5bc1b28bebd
MalcevDanil3131 <strawmorrron666@gmail.com>
2025-11-01 01:19:15 +0500

    Task7

---
+++ Tyuiu.MalcevDV.Sprint3.Task7.V26.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.MalcevDV.Sprint3.Task7.V26.Lib;
+namespace Tyuiu.MalcevDV.Sprint3.Task7.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var ds = new DataService();
+            int startValue = -5; int endValue = 5;
+            int len = endValue - startValue + 1;
+            double[] valueWaitArray = new double[len];
+            valueWaitArray[0] = 19.81;
+            valueWaitArray[1] = 16.79;
+            valueWaitArray[2] = 13.87;
+            valueWaitArray[3] = 10.98;
+            valueWaitArray[4] = 7.94;
+            valueWaitArray[5] = 3.00;
+            valueWaitArray[6] = 3.23;
+            valueWaitArray[7] = -0.45;
+            valueWaitArray[8] = -3.79;
+            valueWaitArray[9] = -6.97;
+            valueWaitArray[10] = -10.00;
+            double[] res = new double[len];
+            res = ds.GetMassFunction(startValue, endValue);
+            CollectionAssert.AreEqual(valueWaitArray, res);
+
+        }
+    }
+}
---
+++ Tyuiu.MalcevDV.Sprint3.Task7.V26/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.MalcevDV.Sprint3.Task7.V26.Lib;
+void PrintCenteredLine(string text, int totalWidth)
+{
+    var padding = totalWidth - text.Length - 2; // -2 для звездочек по бокам
+    Console.WriteLine($"* {text}{new string(' ', padding)}*");
+}
+
+var width = 75;
+
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("Спринт #3", width);
+PrintCenteredLine("Тема: Создание решения по спринту", width);
+PrintCenteredLine("Задание #3", width);
+PrintCenteredLine("Вариант #23", width);
+PrintCenteredLine("Выполнил: Мальцев Данил Вячеславович | РППБ-25-1", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("УСЛОВИЕ:", width);
+PrintCenteredLine("Написать программу, которая заменяет символы в строке", width);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("ИСХОДНЫЕ ДАННЫЕ:", width);
+var ds = new DataService();
+int startValue = -5; int stopValue = 5;
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага= " + stopValue);
+int len = ds.GetMassFunction(startValue, stopValue).Length;
+double[] res = new double[len];
+res = ds.GetMassFunction(startValue, stopValue);
+Console.WriteLine(new string('*', width));
+PrintCenteredLine("РЕЗУЛЬТАТ:", width);
+Console.WriteLine(new string('*', width));
+Console.WriteLine("+----------+-----------+");
+Console.WriteLine("|    X     |   f(x)    |");
+Console.WriteLine("+----------+-----------+");
+for (int i = 0; i < len; i++)
+{
+    Console.WriteLine("|{0,5:d}     |  {1,6:f2}   |", startValue, res[i]);
+    startValue++;
+}
+Console.WriteLine("+----------+-----------+");
+Console.WriteLine(new string('*', width));
